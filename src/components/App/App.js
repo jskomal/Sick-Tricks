57aea9ad95517d0316ea89a4be22f59e67b799b8
Jordan Skomal <jskomal@gmail.com>
2022-04-06 09:22:51 -0600

    Implement fetch for the tricks endpoint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,35 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { tricks: null, errorMsg: '' }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then((res) => {
+        if (!res.ok) {
+          this.setState({ errorMsg: 'Something went wrong please try again later' })
+        } else {
+          return res.json()
+        }
+      })
+      .then((data) => {
+        this.setState({ tricks: data }, () => {
+          console.log(this.state.tricks)
+        })
+      })
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>Sick Trick Wish List</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
